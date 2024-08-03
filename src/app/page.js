58aee99c4909f545ecf1ffd2df56604d10e396b7
SOUMOJIT-CHAKRABORTY <goutamghambir2018@gmail.com>
2024-08-03 15:08:10 +0530

    proto done

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,273 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect } from "react";
+
+// Modal Component
+function Modal({ show, onClose, children }) {
+  if (!show) return null;
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md relative">
+        <button
+          className="absolute top-2 right-2 text-gray-600 hover:text-gray-900"
+          onClick={onClose}
+        >
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
+  const [employees, setEmployees] = useState([]);
+  const [newEmployee, setNewEmployee] = useState({
+    employeeName: "",
+    designation: "",
+    basicSalary: "",
+    houseRent: "",
+    joiningDate: "",
+    phoneNumber: "",
+    dateOfBirth: "",
+  });
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    // Fetch employee data
+    fetch("https://attendancemaker.onrender.com/employees")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((employeeData) => {
+        const formattedData = employeeData.map((emp) => ({
+          id: emp._id, // Ensure the ID is correctly formatted
+          name: emp.employeeName,
+          position: emp.designation,
+          basicSalary: emp.basicSalary,
+          houseRent: emp.houseRent,
+          joiningDate: emp.joiningDate,
+          attendance: "Absent", // Default value for attendance
+        }));
+
+        // Fetch attendance status for each employee
+        const attendancePromises = formattedData.map((employee) =>
+          fetch(
+            `https://attendancemaker.onrender.com/attendanceStatus?employeeId=${employee.id}`
+          )
+            .then((response) => response.json())
+            .then((attendanceData) => ({
+              ...employee,
+              attendance: attendanceData.status,
+            }))
+        );
+
+        Promise.all(attendancePromises)
+          .then((updatedEmployees) => {
+            setEmployees(updatedEmployees);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error fetching attendance status:", error);
+            setLoading(false);
+          });
+      })
+      .catch((error) => {
+        console.error("Error fetching employee data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleAddEmployee = () => {
+    fetch("https://attendancemaker.onrender.com/addEmployee", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        employeeName: newEmployee.employeeName,
+        designation: newEmployee.designation,
+        phoneNumber: newEmployee.phoneNumber,
+        dateOfBirth: newEmployee.dateOfBirth,
+        joiningDate: newEmployee.joiningDate,
+        activeEmployee: true,
+        basicSalary: newEmployee.basicSalary,
+        houseRent: newEmployee.houseRent,
+        medicalAllowance: 0,
+        providentFund: 0,
+        salary:
+          parseFloat(newEmployee.basicSalary) +
+          parseFloat(newEmployee.houseRent),
+        address: "",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "Employee saved successfully") {
+          const newEmployeeData = data.employee; // Use the newly created employee data from the response
+          setEmployees([
+            ...employees,
+            {
+              id: newEmployeeData._id.$oid, // Ensure the ID is correctly formatted
+              name: newEmployeeData.employeeName,
+              position: newEmployeeData.designation,
+              basicSalary: newEmployeeData.basicSalary,
+              houseRent: newEmployeeData.houseRent,
+              joiningDate: newEmployeeData.joiningDate,
+              attendance: "Absent", // Default value for new employee
+            },
+          ]);
+          setNewEmployee({
+            employeeName: "",
+            designation: "",
+            basicSalary: "",
+            houseRent: "",
+            joiningDate: "",
+            phoneNumber: "",
+            dateOfBirth: "",
+          });
+          setShowModal(false);
+        } else {
+          console.error("Failed to add an employee");
+        }
+      })
+      .catch((error) => console.error("Error adding employee:", error));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-4">
+      <div className="flex justify-between items-center mb-4">
+        <h1 className="text-4xl font-bold">Admin View</h1>
+        <button
+          onClick={() => setShowModal(true)}
+          className="bg-blue-500 text-white p-2 rounded"
+        >
+          Add Employee
+        </button>
+      </div>
+
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <div className="loader"></div>
         </div>
-      </div>
+      ) : (
+        <table className="min-w-full bg-white border">
+          <thead>
+            <tr>
+              <th className="py-2 border">ID</th>
+              <th className="py-2 border">Name</th>
+              <th className="py-2 border">Position</th>
+              <th className="py-2 border">Basic Pay</th>
+              <th className="py-2 border">HRA</th>
+              <th className="py-2 border">Joining Date</th>
+              <th className="py-2 border">Attendance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee) => (
+              <tr key={employee.id}>
+                <td className="py-2 border text-center">{employee.id}</td>
+                <td className="py-2 border">{employee.name}</td>
+                <td className="py-2 border">{employee.position}</td>
+                <td className="py-2 border text-right">
+                  {employee.basicSalary}
+                </td>
+                <td className="py-2 border text-right">{employee.houseRent}</td>
+                <td className="py-2 border text-center">
+                  {employee.joiningDate}
+                </td>
+                <td className="py-2 border text-center">
+                  {employee.attendance}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Modal show={showModal} onClose={() => setShowModal(false)}>
+        <h2 className="text-2xl font-bold mb-4">Add New Employee</h2>
+        <input
+          type="text"
+          placeholder="Name"
+          value={newEmployee.employeeName}
+          onChange={(e) =>
+            setNewEmployee({ ...newEmployee, employeeName: e.target.value })
+          }
+          className="border p-2 mb-2 w-full"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <input
+          type="text"
+          placeholder="Position"
+          value={newEmployee.designation}
+          onChange={(e) =>
+            setNewEmployee({ ...newEmployee, designation: e.target.value })
+          }
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="number"
+          placeholder="Basic Pay"
+          value={newEmployee.basicSalary}
+          onChange={(e) =>
+            setNewEmployee({
+              ...newEmployee,
+              basicSalary: e.target.value,
+            })
+          }
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="number"
+          placeholder="HRA"
+          value={newEmployee.houseRent}
+          onChange={(e) =>
+            setNewEmployee({
+              ...newEmployee,
+              houseRent: e.target.value,
+            })
+          }
+          className="border p-2 mb-2 w-full"
+        />
+        <label className="block mb-2 text-gray-500">Joining Date</label>
+        <input
+          type="date"
+          value={newEmployee.joiningDate}
+          onChange={(e) =>
+            setNewEmployee({ ...newEmployee, joiningDate: e.target.value })
+          }
+          className="border p-2 mb-2 w-full"
+        />
+        <label className="block mb-2 text-gray-500">Date of Birth</label>
+        <input
+          type="date"
+          value={newEmployee.dateOfBirth}
+          onChange={(e) =>
+            setNewEmployee({ ...newEmployee, dateOfBirth: e.target.value })
+          }
+          className="border p-2 mb-2 w-full"
+        />
+        <input
+          type="text"
+          placeholder="Phone Number"
+          value={newEmployee.phoneNumber}
+          onChange={(e) =>
+            setNewEmployee({ ...newEmployee, phoneNumber: e.target.value })
+          }
+          className="border p-2 mb-4 w-full"
+        />
+        <button
+          onClick={handleAddEmployee}
+          className="bg-blue-500 text-white p-2 w-full rounded"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          Add Employee
+        </button>
+      </Modal>
     </main>
   );
 }
